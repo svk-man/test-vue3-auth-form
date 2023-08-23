a8bcda4583a70acdf589a175b51a59ae99893c68
Sergey Kravchenko <kravchenko.sgv@gmail.com>
2023-08-23 17:36:30 +0300

    fix: add styles for error message

--- src/widgets/auth-form/index.vue
+++ src/widgets/auth-form/index.vue
@@ -68,10 +68,10 @@ async function sendCode() {
     <authByPhone.InputCode
       v-model="code"
       v-else />
-    <span
+    <p
       v-if="errorMessage"
-      class="text-sm text-red"
-      >{{ errorMessage }}</span>
+      class="text-sm text-red-400 m-none mt-4"
+      >{{ errorMessage }}</p>
     <div class="mt-5">
       <input type="submit"
         class="bg-indigo-600 text-white text-sm leading-6 font-medium py-2 px-3 rounded-lg w-full cursor-pointer"
